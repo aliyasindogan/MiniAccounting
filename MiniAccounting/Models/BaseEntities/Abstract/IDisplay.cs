ea352b546fd032e123dade1e8d3850a51067992f
Ali Yasin Doğan <a.yasindogan@gmail.com>
2020-04-25 02:42:39 +0300

    IDisplay Silindi.

--- MiniAccounting/Models/BaseEntities/Abstract/IDisplay.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MiniAccounting.Models.BaseEntities.Abstract
-{
-    public interface IDisplay
-    {
-        bool IsDisplay { get; set; }
-        int DisplayOrder { get; set; }
-    }
-}
